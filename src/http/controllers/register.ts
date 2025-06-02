b604cc48fb4e1b167f2759a63aa1d7af0a608338
Tiago C. Lima <casmei@protonmail.com>
2025-06-02 17:41:24 -0300

    ✨ feat: Add bcrypt hashing functionality for user password encryption
    - Added "@types/bcryptjs" v2.4.6 and "bcryptjs" v3.0.2 to package dependencies.
    - Implemented bcrypt hashing in the register function in `register.ts`.
    - Created a new route `/users` in `routes.ts` to handle user registrations and leverage the newly added bcrypt functionality.

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+	const registerBodySchema = z.object({
+		name: z.string(),
+		email: z.string().email(),
+		password: z.string().min(6),
+	});
+
+	const { email, name, password } = registerBodySchema.parse(request.body);
+
+	const userWithSameEamil = await prisma.user.findUnique({ where: { email } });
+
+	if (userWithSameEamil) {
+		return reply.status(409).send();
+	}
+
+	const passoword_hash = await hash(password, 6);
+
+	await prisma.user.create({
+		data: {
+			name,
+			email,
+			passoword_hash,
+		},
+	});
+
+	return reply.status(201).send();
+}
