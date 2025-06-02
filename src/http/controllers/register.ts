acb1d954069a8df56e300a0638e092bae443500f
Tiago C. Lima <casmei@protonmail.com>
2025-06-02 18:13:01 -0300

    💅 style: just apply biome code style

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,19 +3,19 @@ import type { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
-	const registerBodySchema = z.object({
-		name: z.string(),
-		email: z.string().email(),
-		password: z.string().min(6),
-	});
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
 
-	const { email, name, password } = registerBodySchema.parse(request.body);
+  const { email, name, password } = registerBodySchema.parse(request.body);
 
-	try {
-		await registerUseCase({ name, email, password });
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    await registerUseCase({ name, email, password });
+  } catch (error) {
+    console.log(error);
+  }
 
-	return reply.status(201).send();
+  return reply.status(201).send();
 }
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -2,29 +2,29 @@ import { prisma } from "@/lib/prisma";
 import { hash } from "bcryptjs";
 
 interface RegisterUseCaseRequest {
-	name: string;
-	email: string;
-	password: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
 export async function registerUseCase({
-	email,
-	name,
-	password,
+  email,
+  name,
+  password,
 }: RegisterUseCaseRequest) {
-	const userWithSameEamil = await prisma.user.findUnique({ where: { email } });
+  const userWithSameEamil = await prisma.user.findUnique({ where: { email } });
 
-	if (userWithSameEamil) {
-		throw new Error("Email already exists.");
-	}
+  if (userWithSameEamil) {
+    throw new Error("Email already exists");
+  }
 
-	const passoword_hash = await hash(password, 6);
+  const passoword_hash = await hash(password, 6);
 
-	await prisma.user.create({
-		data: {
-			name,
-			email,
-			passoword_hash,
-		},
-	});
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      passoword_hash,
+    },
+  });
 }
